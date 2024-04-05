1019e8c9f33abf919f13c07cb1961c1decf1f2cc
opowell <opowell@gmail.com>
2024-04-05 22:10:40 +0200

    running

--- src/apps/rogue/GameScreen.vue
+++ src/apps/rogue/GameScreen.vue
@@ -1,37 +1,32 @@
 <template>
   <div class="game-screen" @keydown="handleKeydown" tabindex="0" ref="screen">
-    <div
+    <GameLocation
       v-for="location in visibleLocations"
       :key="location.x + '-' + location.y"
-      :style="locationStyle(location)"
-      class="location"
-      :class="locationClasses(location)"
-      v-html="locationContent(location)"
+      :location="location"
     />
   </div>
 </template>
 <script>
+import GameLocation from './GameLocation.vue'
 const LOCATION = {
   WIDTH: 14,
   HEIGHT: 22.4
 }
 export default {
   name: 'GameScreen',
+  components: {
+    GameLocation
+  },
   props: {
     game: { type: Object, required: true }
-  },
-  data() {
-    return {
-      locationWidth: LOCATION.WIDTH + 'px',
-      locationHeight: LOCATION.HEIGHT + 'px'
-    }
   },
   computed: {
     locations() {
       return this.game.locations.flat()
     },
     visibleLocations() {
-      return this.locations.filter(location => location.visible || location.room?.lit)
+      return this.locations.filter(location => location.visible || location.mapped)
     },
     screenWidth() {
       return this.game.width * LOCATION.WIDTH + 'px'
@@ -44,60 +39,20 @@ export default {
     this.$refs.screen.focus()
   },
   methods: {
-    locationClasses(location) {
-      const classes = {}
-      if (location.item?.type === 'staircase' && !location.character) {
-        classes.flashing = true
-      }
-      return classes
-    },
-    locationContent(location) {
-      if (location.character) {
-        return '@'
-      }
-      if (location.item) {
-        switch (location.item.type) {
-          case 'stick':
-            return '&#x03B6;'
-          case 'scroll':
-            return '&#x266A;'
-          case 'potion':
-            return '&#x0021;'
-          case 'weapon':
-            return '&#x2191;'
-          case 'ring':
-            return '&#x25CB;'
-          case 'gold':
-            return '&#x273D;'
-          case 'staircase':
-            return '&#x2630;'
-        }
-      }
-      switch (location.type) {
-        case 'hallway':
-          return ''
-        case 'floor':
-          return '&#8231;'
-        case 'upLeftWall':
-          return '&#x255D;'
-        case 'upRightWall':
-          return '&#x255A;'
-        case 'downLeftWall':
-          return '&#x2557;'
-        case 'downRightWall':
-          return '&#x2554;'
-        case 'horizontalWall':
-          return '&#x2550;'
-        case 'verticalWall':
-          return '&#x2551;'
-        case 'door':
-          return '&#x256C;'
-        default:
-          return ''
-      }
-    },
     handleKeydown(event) {
       switch (event.key) {
+        case 'H':
+          this.game.runLeft()
+          break
+        case 'J':
+          this.game.runDown()
+          break
+        case 'K':
+          this.game.runUp()
+          break
+        case 'L':
+          this.game.runRight()
+          break
         case 'h':
           this.game.moveLeft()
           break
@@ -123,56 +78,6 @@ export default {
           this.game.moveDownRight()
           break
       }
-    },
-    getBackgroundColor(location) {
-      if (location.character) {
-        if (location.type === 'hallway') {
-          return '#b3b3b3'
-        }
-        return 'black'
-      }
-      if (location.item) {
-        switch (location.item.type) {
-          case 'staircase': {
-            return 'lightgreen'
-          }
-        }
-      }
-      if (location.type === 'hallway') {
-        return 'grey'
-      }
-      return ''
-    },
-    getColor(location) {
-      if (location.character) {
-        return 'yellow'
-      }
-      if (location.item) {
-        switch (location.item.type) {
-          case 'ring':
-          case 'weapon':
-          case 'stick':
-          case 'potion':
-          case 'scroll':
-            return '#5555ff'
-          case 'gold':
-            return '#ffff05'
-          case 'staircase': {
-            return 'black'
-          }
-        }
-      }
-      if (location.type === 'floor') return '#00ff34'
-      if (location.type !== 'floor') return '#b74f00'
-      return
-    },
-    locationStyle(location) {
-      return {
-        color: this.getColor(location),
-        'background-color': this.getBackgroundColor(location),
-        top: location.y * LOCATION.HEIGHT + 'px',
-        left: location.x * LOCATION.WIDTH + 'px',
-      }
     }
   }
 }
@@ -184,25 +89,5 @@ export default {
   background-color: black;
   position: relative;
   font-family: 'Courier New';
-}
-.location {
-  width: v-bind(locationWidth);
-  height: v-bind(locationHeight);
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: v-bind(locationHeight);
-}
-@keyframes example {
-  0%   {color: transparent;}
-  49%  {color:transparent;}
-  50% {color:black;}
-  100% {color:black;}
-}
-.flashing {
-  animation-name: example;
-  animation-duration: 1s;
-  animation-iteration-count: infinite;
 }
 </style>
