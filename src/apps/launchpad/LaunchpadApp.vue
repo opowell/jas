82d8d2399d72e5ed9e9694af284615b145b5e81b
opowell <opowell@gmail.com>
2024-04-15 22:12:38 +0200

    add colors to launchpad

--- src/apps/launchpad/LaunchpadApp.vue
+++ src/apps/launchpad/LaunchpadApp.vue
@@ -5,10 +5,33 @@
   </div>
 </template>
 <script>
+function djb2(str) {
+  var hash = 5381
+  for (var i = 0; i < str.length; i++) {
+    hash = ((hash << 5) + hash) + str.charCodeAt(i) /* hash * 33 + c */
+  }
+  return hash;
+}
+
+function hashStringToColor(str) {
+  var hash = djb2(str)
+  var g = (hash & 0xFF0000) >> 16
+  var b = (hash & 0x00FF00) >> 8
+  var r = hash & 0x0000FF
+  return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2)
+}
 export default {
   name: 'LaunchpadApp',
   props: {
     app: { type: String, required: true }
+  },
+  computed: {
+    bgColor() {
+      return hashStringToColor(this.app)
+    },
+    fadedBgColor() {
+      return this.bgColor + '4d'
+    }
   }
 }
 </script>
@@ -22,16 +45,17 @@ export default {
   border-radius: 0.5rem;
 }
 .app:hover {
-  background-color: #88337769;
+  background-color: v-bind(fadedBgColor);
   cursor: pointer;
 }
 .app-icon {
-  background-color: #c5aaaa;
+  background-color: v-bind(bgColor);
   height: 9rem;
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 5rem;
+  opacity: 1;
 }
 .app-name {
   color: white;
