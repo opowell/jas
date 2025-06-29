292ae56a103b124a38b5d5b9c2c56ba8b9219b3d
opowell <opowell@gmail.com>
2025-06-30 00:44:43 +0200

    - generate plist file dynamically (use current location of install.sh).

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/sh -e
-plist_path="localhost.jas.plist"
-plist_filename=$(basename "$plist_path")
+echo 'JAS - installing...'
+plist_filename="localhost.jas.plist"
 install_path="/Users/$USER/Library/LaunchAgents/$plist_filename"
 
-echo "installing launchctl plist: server/$plist_path --> $install_path"
-cp -f "server/$plist_path" "$install_path"
+rm -f "server/$plist_filename"
+cp "server/template-$plist_filename" "server/$plist_filename"
+sed -i '' "s|JAS_HOME|$PWD|g" "server/localhost.jas.plist"
+cp -f "server/$plist_filename" "$install_path"
 # sudo chown $USER "$install_path"
 # sudo chmod 644 "$install_path"
 
 launchctl unload -w "$install_path"
 launchctl load -w "$install_path"
 
-echo "- to check if it's running, run: sudo launchctl list | grep jas"
-echo "- to uninstall, run: sudo launchctl unload \"$install_path\""
+
+echo 'JAS - COMPLETE.'
+# echo "- to check if it's running, run: sudo launchctl list | grep jas"
+# echo "- to uninstall, run: sudo launchctl unload \"$install_path\""
